a24b782b0606d53385b5a4c59ad04ce564337b49
JoyfulOne <lile20220429@gmail.com>
2026-07-18 16:09:54 +0800

    修改过后的代码

--- Basic/language/LoopStructure/src/求出e的值.c
+++ Basic/language/LoopStructure/src/求出e的值.c
@@ -2,20 +2,14 @@
 
 int main()
 {
-    int n, temp;
-    double e = 1;
+    int n;
+    double e = 1, temp = 1;
     scanf("%d", &n);
 
     for(int i = 1; i <= n; i++)
     {
-        temp = 1;
-
-        for(int j = 1; j <= i; j++)
-        {
-            temp *= j;
-        }
-
-        e += 1.0 / temp;
+        temp /= i;
+        e += temp;
     }
 
     printf("%.10lf", e);
--- Basic/language/LoopStructure/src/第n小的质数.c
+++ Basic/language/LoopStructure/src/第n小的质数.c
@@ -2,15 +2,23 @@
 
 int main()
 {
-    int n, position = 1;
+    int n, position = 0;
     scanf("%d", &n);
 
     for(int i = 2; ; i++)
     {
-        for(int j = 2; j < i; j++)
+        int isprime = 1;
+        for(int j = 2; j * j <= i; j++)
         {
-            if(i % j == 0) break;
-            if(j == i - 1) position++;
+            if(i % j == 0)
+            {
+                isprime = 0;
+                break;
+            }
+        }
+        if(isprime)
+        {
+            position++;
         }
 
         if(position == n) 
